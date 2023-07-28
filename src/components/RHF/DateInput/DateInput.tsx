6a3e966bc2df3b5f53815aa751ed86a9be8449d4
alex <arevaloalex9@gmail.com>
2023-07-27 21:48:57 -0500

    fix storybook bugs

--- src/components/RHF/DateInput/DateInput.tsx
+++ src/components/RHF/DateInput/DateInput.tsx
@@ -1,6 +1,12 @@
 import clsx from "clsx";
 import React, { type ComponentProps } from "react";
-import { Controller, useFormContext } from "react-hook-form";
+import {
+  Controller,
+  useFormContext,
+  type ControllerRenderProps,
+  type FieldError,
+  type FieldValues,
+} from "react-hook-form";
 
 interface IDateInputProps {
   fullWidth?: boolean;
@@ -11,7 +17,52 @@ type InputProps = IDateInputProps &
   ComponentProps<"input"> & {
     labelText?: string;
     helperText?: string;
+    field: ControllerRenderProps<FieldValues, string>;
+    error: FieldError | undefined;
   };
+
+export function DateInputStory({
+  fullWidth,
+  labelText,
+  helperText,
+  id,
+  name,
+  field,
+  error,
+  ...rest
+}: InputProps) {
+  return (
+    <div>
+      {labelText && (
+        <label
+          htmlFor={id}
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          {labelText}
+        </label>
+      )}
+      <input
+        id={id}
+        type="date"
+        className={clsx(
+          fullWidth && "w-full",
+          "block rounded-md border-0 px-6 py-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+        )}
+        {...rest}
+        {...field}
+      />
+      <p
+        className={clsx(
+          error ? "text-red-600" : "text-gray-500",
+          "mt-2 text-sm"
+        )}
+        id={`${name ?? ""}-error`}
+      >
+        {error ? error?.message : helperText}
+      </p>
+    </div>
+  );
+}
 
 export default function DateInput({
   fullWidth,
@@ -20,7 +71,7 @@ export default function DateInput({
   id,
   name,
   ...rest
-}: InputProps) {
+}: Omit<InputProps, "error" | "field">) {
   const { control } = useFormContext();
 
   return (
@@ -28,35 +79,16 @@ export default function DateInput({
       name={name ?? ""}
       control={control}
       render={({ field, fieldState: { error } }) => (
-        <div>
-          {labelText && (
-            <label
-              htmlFor={id}
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              {labelText}
-            </label>
-          )}
-          <input
-            id={id}
-            type="date"
-            className={clsx(
-              fullWidth && "w-full",
-              "block rounded-md border-0 px-6 py-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
-            )}
-            {...rest}
-            {...field}
-          />
-          <p
-            className={clsx(
-              error ? "text-red-600" : "text-gray-500",
-              "mt-2 text-sm"
-            )}
-            id={`${name ?? ""}-error`}
-          >
-            {error ? error?.message : helperText}
-          </p>
-        </div>
+        <DateInputStory
+          fullWidth={fullWidth}
+          labelText={labelText}
+          helperText={helperText}
+          id={id}
+          name={name}
+          field={field}
+          error={error}
+          {...rest}
+        />
       )}
     />
   );
--- src/components/RHF/RadioGroup/RadioGroup.tsx
+++ src/components/RHF/RadioGroup/RadioGroup.tsx
@@ -1,6 +1,11 @@
 import clsx from "clsx";
 import { RadioGroup } from "@headlessui/react";
-import { Controller, useFormContext } from "react-hook-form";
+import {
+  Controller,
+  useFormContext,
+  type ControllerRenderProps,
+  type FieldValues,
+} from "react-hook-form";
 
 type Radio = {
   value: string;
@@ -13,6 +18,74 @@ interface IRadioGroupProps {
   options: Radio[];
   alignCenter?: boolean;
   flexRow?: boolean;
+  field: ControllerRenderProps<FieldValues, string>;
+}
+
+export function RadioGroupStory({
+  options,
+  alignCenter,
+  name,
+  flexRow,
+  field,
+}: IRadioGroupProps) {
+  return (
+    <RadioGroup value={field?.value} onChange={field?.onChange} name={name}>
+      <div
+        className={clsx(flexRow ? "flex justify-center gap-4" : "space-y-4")}
+      >
+        {options.map((option) => (
+          <RadioGroup.Option
+            key={option.value}
+            value={option.value}
+            className={({ checked, active }) =>
+              clsx(
+                checked ? "border-transparent bg-zinc-200" : "border-gray-300",
+                active ? "border-gray-600 ring-2 ring-gray-600" : "",
+                "relative block cursor-pointer rounded-lg border bg-white px-6 py-4 shadow-sm focus:outline-none sm:flex sm:justify-between",
+                flexRow && "grow"
+              )
+            }
+          >
+            {({ active, checked }) => (
+              <>
+                <span
+                  className={clsx(
+                    "flex items-center",
+                    alignCenter && "w-full justify-center text-center"
+                  )}
+                >
+                  <span className="flex flex-col text-sm">
+                    <RadioGroup.Label
+                      as="span"
+                      className="text-base font-medium text-gray-900"
+                    >
+                      {option.label}
+                    </RadioGroup.Label>
+                    {option.description ? (
+                      <RadioGroup.Description
+                        as="span"
+                        className="text-gray-500"
+                      >
+                        {option.description}
+                      </RadioGroup.Description>
+                    ) : null}
+                  </span>
+                </span>
+                <span
+                  className={clsx(
+                    active ? "border" : "border-2",
+                    checked ? "border-gray-800" : "border-transparent",
+                    "pointer-events-none absolute -inset-px rounded-lg"
+                  )}
+                  aria-hidden="true"
+                />
+              </>
+            )}
+          </RadioGroup.Option>
+        ))}
+      </div>
+    </RadioGroup>
+  );
 }
 
 export default function RadioGroupComponent({
@@ -20,7 +93,7 @@ export default function RadioGroupComponent({
   alignCenter,
   name,
   flexRow,
-}: IRadioGroupProps) {
+}: Omit<IRadioGroupProps, "error" | "field">) {
   const { control } = useFormContext();
 
   return (
@@ -28,66 +101,13 @@ export default function RadioGroupComponent({
       name={name}
       control={control}
       render={({ field }) => (
-        <RadioGroup value={field.value} onChange={field.onChange} name={name}>
-          <div
-            className={clsx(
-              flexRow ? "flex justify-center gap-4" : "space-y-4"
-            )}
-          >
-            {options.map((option) => (
-              <RadioGroup.Option
-                key={option.value}
-                value={option.value}
-                className={({ checked, active }) =>
-                  clsx(
-                    checked
-                      ? "border-transparent bg-zinc-200"
-                      : "border-gray-300",
-                    active ? "border-gray-600 ring-2 ring-gray-600" : "",
-                    "relative block cursor-pointer rounded-lg border bg-white px-6 py-4 shadow-sm focus:outline-none sm:flex sm:justify-between",
-                    flexRow && "grow"
-                  )
-                }
-              >
-                {({ active, checked }) => (
-                  <>
-                    <span
-                      className={clsx(
-                        "flex items-center",
-                        alignCenter && "w-full justify-center text-center"
-                      )}
-                    >
-                      <span className="flex flex-col text-sm">
-                        <RadioGroup.Label
-                          as="span"
-                          className="text-base font-medium text-gray-900"
-                        >
-                          {option.label}
-                        </RadioGroup.Label>
-                        {option.description ? (
-                          <RadioGroup.Description
-                            as="span"
-                            className="text-gray-500"
-                          >
-                            {option.description}
-                          </RadioGroup.Description>
-                        ) : null}
-                      </span>
-                    </span>
-                    <span
-                      className={clsx(
-                        active ? "border" : "border-2",
-                        checked ? "border-gray-800" : "border-transparent",
-                        "pointer-events-none absolute -inset-px rounded-lg"
-                      )}
-                      aria-hidden="true"
-                    />
-                  </>
-                )}
-              </RadioGroup.Option>
-            ))}
-          </div>
-        </RadioGroup>
+        <RadioGroupStory
+          options={options}
+          alignCenter={alignCenter}
+          name={name}
+          flexRow={flexRow}
+          field={field}
+        />
       )}
     />
   );
